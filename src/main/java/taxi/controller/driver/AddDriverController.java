84daf398383cc5594b1919d0fac6a5f00b5b8325
Vova Bilodon <vovabilodon@gmail.com>
2021-07-28 13:09:56 +0300

    implemented controllers; added jsp files for controllers; configured web.xml;

---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,34 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String licenceNumber = req.getParameter("licence_number");
+        Driver driver = new Driver(name, licenceNumber, password, login);
+        driverService.create(driver);
+        resp.sendRedirect("/drivers/add");
+    }
+}
